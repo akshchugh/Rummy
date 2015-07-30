cbba3dcfa9a2f2951dcf573a0dcaee2c432cd369
abilng <mail@abilng.in>
2015-07-30 04:51:33 -0700

    Added: Dummy Dealer Class

---
+++ src/Dealer.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Dealer {
+
+	
+	public Dealer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Hand getHand() {
+		Hand hand = null;
+		
+		
+		return hand;
+	}
+
+}
